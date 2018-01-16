384987ec9b063466168e60762ace83ac752a352f
Kcunningham20 <31940556+Kcunningham20@users.noreply.github.com>
2018-01-16 15:33:06 +0000

    Slowed down speed

--- Racer/config.js
+++ Racer/config.js
@@ -9,9 +9,9 @@ const player2Name = 'Player 2';
 
 const track_friction = 0.05; // Smaller number means more slippery surface
 
-const player_mass = 1; // Larger number means it takes longer to accelerate to top speed
+const player_mass = 3; // Larger number means it takes longer to accelerate to top speed
 
-const player_move_speed = 20; // Bigger number = faster!
+const player_move_speed = 10; // Bigger number = faster!
 
 const player_top_speed = 25;
 const player_rotate_speed = 0.4; // Bigger number = faster!
